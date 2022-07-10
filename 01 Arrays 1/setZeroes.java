30faf4e25d96d8e11a38f0fece31c436e54ea6a2
saivishesh <saivishesh74@gmail.com>
2022-07-10 13:13:55 +0530

    First Problem

---
+++ 01 Arrays 1/setZeroes.java
@@ -0,0 +1,78 @@
+ public void setZeroes(int[][] matrix) {
+      //----------------------------------------//
+      // Better Approach//
+        int n=matrix.length,m=matrix[0].length;
+       
+        boolean [] row = new boolean[n];
+        boolean [] col = new boolean[m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row[i]=true;
+                
+                    col[j]=true;
+                }
+            }
+        }
+       for(int i=0;i<n;i++)
+       {
+            if(row[i])
+            {
+                
+                    for(int k=0;k<m;k++)
+                        matrix[i][k]=0;
+               
+            
+            }
+       }
+        for(int i=0;i<m;i++)
+        {
+             if(col[i])
+             {
+                 
+                     for(int k=0;k<n;k++)
+                        matrix[k][i]=0;
+             }
+        }
+        
+        
+        
+        
+        
+        
+        
+        //------------------------------------------------//
+        // Final Approach 
+        
+        int n=matrix.length,m=matrix[0].length;
+        int col=1;
+      
+        for(int i=0;i<n;i++)
+        {
+            if(matrix[i][0]==0)
+                col=0;
+            for(int j=1;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=1;j--)
+            {
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j]=0;
+            }
+          if(col==0)
+            matrix[i][0]=0;
+        }
+       
+     
+    }
