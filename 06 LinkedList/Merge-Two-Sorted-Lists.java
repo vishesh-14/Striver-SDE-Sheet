8bb7bfd0c72851cc7e06ec95801d88c76d6e8f4e
vishesh-14 <63588291+vishesh-14@users.noreply.github.com>
2022-07-19 12:15:24 +0530

    third problem

--- 06 LinkedList/Merge-Two-Sorted-Lists.java
+++ 06 LinkedList/Merge-Two-Sorted-Lists.java
@@ -24,3 +24,26 @@ public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
              prev.next=q;
         return dummy.next;
     }
+
+public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Time Complexity : o(n+m)
+        // Space Complexity : o(stack space i.e n+m)
+          return mergeRecursive(list1,list2);
+    }
+    public  ListNode mergeRecursive(ListNode list1,ListNode list2)
+    {
+        if(list1==null)
+            return list2;
+        if(list2==null)
+            return list1;
+         if(list1.val<=list2.val)
+         {
+               list1.next=mergeRecursive(list1.next,list2);
+             return list1;
+         }
+        else
+        {
+            list2.next=mergeRecursive(list1,list2.next);
+            return list2;
+        }
+    }
