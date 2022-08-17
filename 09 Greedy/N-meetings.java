6ec3b1ee1a83304ad100f8534cec4e822251de3a
saivishesh <saivishesh74@gmail.com>
2022-08-17 11:47:26 +0530

    first problem

--- 09 Greedy/N-meetings.java
+++ 09 Greedy/N-meetings.java
@@ -46,3 +46,37 @@ class Node implements Comparable<Node>
          return this.f-obj.f;
     }
 }
+class Solution
+{
+    //Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int W, Item arr[], int n) 
+    {
+        // Your code here
+        Arrays.sort(arr,new mycomp());
+         double maxAmount=0;
+         for(int i=0;i<n;i++)
+         {
+              if(W>=arr[i].weight)
+              {
+                  maxAmount+=arr[i].value;
+                  W-=arr[i].weight;
+              }
+              else 
+              {
+                   double ratio=(arr[i].value)/(double)arr[i].weight;
+                   maxAmount+=ratio*W;
+                   break;
+              }
+         }
+         return maxAmount;
+    }
+}
+class mycomp implements Comparator<Item>
+{
+     public int compare(Item i1, Item i2)
+     {
+         double ratio1=(i1.value)/(double)i1.weight;
+         double ratio2=(i2.value)/(double)i2.weight;
+         return (int)(ratio2-ratio1);
+     }
+}
---
+++ 12 Binary Search/N-th-root.java
@@ -0,0 +1,26 @@
+double multiply(double val,int n);
+double findNthRootOfM(int n, int m) {
+    double low = 1;
+        double high = m;
+        double eps = 1e-9; 
+        
+        while((high - low) > eps) {
+            double mid = low+( high-low) / 2.0; 
+            if(multiply(mid, n) <=m) {
+                low = mid; 
+            }
+            else {
+                high = mid; 
+            }
+        }
+        return low;
+ 
+ }
+double multiply(double number,int n)
+{
+     double ans = 1.0;
+        for(int i = 1;i<=n;i++) {
+            ans = ans * number;
+        }
+    return ans;
+}
