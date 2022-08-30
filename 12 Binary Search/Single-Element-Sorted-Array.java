6ac1a11c912ef29c967461c2839102a8ddfcc561
vishesh-14 <63588291+vishesh-14@users.noreply.github.com>
2022-08-30 14:16:53 +0530

    third problem

--- 12 Binary Search/Single-Element-Sorted-Array.java
+++ 12 Binary Search/Single-Element-Sorted-Array.java
@@ -16,4 +16,40 @@ public int singleNonDuplicate(int[] nums) {
                 high=mid-1; 
         }
         return nums[low];
+        
+       //-----------------------------------------------------------//
+       //-------- another approach-------------------------------//
+          int n=nums.length; 
+        if(n==1)
+             return nums[0];
+        if(nums[0]!=nums[1])
+            return nums[0];
+        if(nums[n-1]!=nums[n-2])
+            return nums[n-1];
+        int low=1,high=nums.length-2,ans=-1;
+      while(low<=high)
+      {
+           int mid=(low+high)/2;
+          if(mid%2==0)
+          {
+               if(nums[mid]==nums[mid+1])
+                   low=mid+1;
+              else
+              {
+                  ans=nums[mid];
+                  high=mid-1;
+              }
+          }
+          else
+          {
+               if(nums[mid]==nums[mid-1])
+                   low=mid+1;
+              else
+              {
+                  ans=nums[mid];
+                  high=mid-1;
+              }
+          }
+      }
+        return ans;
     }
