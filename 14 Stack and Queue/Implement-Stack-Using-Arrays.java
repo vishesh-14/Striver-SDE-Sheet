ff1c825286e13b55fbee8a4446c0e1df47c75df7
saivishesh <saivishesh74@gmail.com>
2022-09-14 11:48:26 +0530

    first problem

---
+++ 14 Stack and Queue/Implement-Stack-Using-Arrays.java
@@ -0,0 +1,43 @@
+
+public class Stack {
+    int size=-1,top=-1;
+    int [] arr;
+    Stack(int size)
+    {
+        this.size=size;
+        arr=new int[size];
+     
+    }
+    void push(int num) {
+        // Write your code here.
+        if(top==size)
+            return;
+        arr[++top]=num;
+    }
+    int pop() {
+        // Write your code here.
+        if(top==-1)
+            return -1;
+         int ele=arr[top--];
+        
+        return ele;
+    }
+    int top() {
+        // Write your code here.
+        if(top==-1)
+           return -1;
+        return arr[top];
+    }
+    int isEmpty() {
+        // Write your code here.
+        if(top==-1)
+            return 1;
+        return 0;
+    }
+    int isFull() {
+        // Write your code here.
+        if(top==size)
+            return 1;
+        return 0;
+    }
+}
