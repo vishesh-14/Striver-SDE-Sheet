c56040e2b0b832fd4078ed2863d0f40e869356e5
vishesh-14 <63588291+vishesh-14@users.noreply.github.com>
2022-09-19 10:59:59 +0530

    fourth problem

---
+++ 15 Stack and Queue 2/Sliding-Window-Maximum.java
@@ -0,0 +1,24 @@
+public int[] maxSlidingWindow(int[] nums, int k) {
+        // Time Complexity : o(2*n)
+        // Space Complexity : o(k)
+         int n=nums.length,idx=0;
+        Deque<Integer> dq=new LinkedList<Integer>();
+        int [] maximums=new int[n-k+1];
+         for(int i=0;i<k;i++)
+         {
+             while(!dq.isEmpty() && nums[i]>=nums[dq.peekLast()])
+                 dq.pollLast();
+             dq.offerLast(i);
+         }
+        maximums[idx++]=nums[dq.peekFirst()];
+        for(int i=k;i<n;i++)
+        {
+             while(!dq.isEmpty() && dq.peek()<=i-k)
+                 dq.pollFirst();
+             while(!dq.isEmpty() && nums[i]>=nums[dq.peekLast()])
+                 dq.pollLast();
+            dq.offerLast(i);
+            maximums[idx++]=nums[dq.peekFirst()];
+        }
+        return maximums;
+    }
