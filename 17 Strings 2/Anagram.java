6c74fc942fecc5887691d295be4e667fb3fe89f2
saivishesh <saivishesh74@gmail.com>
2022-09-26 12:10:16 +0530

    first problem

---
+++ 17 Strings 2/Anagram.java
@@ -0,0 +1,19 @@
+public boolean isAnagram(String s, String t) {
+        int n=s.length(),m=t.length();
+        if(n!=m)
+            return false;
+        // Time Complexity : o(n+26)
+        // Space Complexity : o(26)
+        int [] c = new int[26];
+        for(int i=0;i<n;i++)
+        {
+            c[s.charAt(i)-'a']++;
+            c[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++)
+        {
+             if(c[i]!=0)
+                 return false;
+        }
+        return true;
+    }
