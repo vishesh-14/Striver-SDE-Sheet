711f863755369543633096dd2667d04da8b19ba8
vishesh-14 <63588291+vishesh-14@users.noreply.github.com>
2022-10-04 13:06:46 +0530

    third problem

--- 18 Binary Tree/Postorder-Traversal.java
+++ 18 Binary Tree/Postorder-Traversal.java
@@ -8,3 +8,32 @@ public static void postOrderRecursive(TreeNode root)
           postOrderRecursive(root.right);
           order.add(root.val);
     }
+ //-------------------------------------------------------------------//
+  public static void  postOrderIterative(TreeNode root)
+    {
+        // Time Complexity: o(n)
+        // Space Complexity: o(n+n)
+         if(root==null)
+             return;
+        ArrayDeque<TreeNode> st1=new ArrayDeque<TreeNode>();
+        ArrayDeque<TreeNode> st2=new ArrayDeque<TreeNode>();
+        st1.push(root);
+         while(!st1.isEmpty())
+         {
+             TreeNode top=st1.peek();
+             st1.pop();
+             
+              st2.push(top);
+              if(top.left!=null)
+                  st1.push(top.left);
+               if(top.right!=null)
+                   st1.push(top.right);
+             
+         }
+        while(!st2.isEmpty())
+        {
+            TreeNode top=st2.peek();
+            st2.pop();
+            order.add(top.val);
+        }
+    }
