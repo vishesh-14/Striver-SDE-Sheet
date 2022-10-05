7f6de016122b496a5281eb0aab852a28e925a004
vishesh-14 <63588291+vishesh-14@users.noreply.github.com>
2022-10-05 17:01:33 +0530

    eigth problem

---
+++ 18 Binary Tree/Top-View-of-Binary-Tree.java
@@ -0,0 +1,47 @@
+class Solution
+{
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    static ArrayList<Integer> topView(Node root)
+    {
+        
+        // Time Complexity : o(n)
+        // Space Complexity : o(n)
+        TreeMap<Integer,Integer> map =new TreeMap<Integer,Integer>();
+        Queue<Pair> q =new LinkedList<Pair>();
+         Pair temp=new Pair(root,0);
+         q.offer(temp);
+         while(!q.isEmpty())
+         {
+              int size=q.size();
+              for(int i=0;i<size;i++)
+              {
+                   Pair front=q.poll();
+                   if(map.get(front.line)==null)
+                   map.put(front.line,front.nm.data);
+                    if(front.nm.left!=null)
+                     q.offer(new Pair(front.nm.left,front.line-1));
+                    if(front.nm.right!=null)
+                    q.offer(new Pair(front.nm.right,front.line+1));
+              }
+         }
+        
+          ArrayList<Integer> result=new ArrayList<Integer>();
+          for(Map.Entry it : map.entrySet())
+           result.add((int)it.getValue());
+           return result;
+    }
+   
+}
+class Pair
+{
+   Node nm;
+   int line;
+    Pair(Node nm,int line)
+    {
+         this.nm=nm;
+         this.line=line;
+    }
+    
+    
+}
