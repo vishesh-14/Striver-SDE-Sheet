6993ac6efd284c28f2bcef577441fb3f93952abb
vishesh-14 <63588291+vishesh-14@users.noreply.github.com>
2022-10-06 12:51:13 +0530

    ninth problem

---
+++ 18 Binary Tree/Vertical-Order-Traversal.java
@@ -0,0 +1,38 @@
+ public List<List<Integer>> verticalTraversal(TreeNode root) {
+        // Time Complexity : o(n)
+        // Space Complexity : o(n+n)
+          TreeMap<Integer,ArrayList<Integer>> map = new TreeMap<Integer,ArrayList<Integer>>(); 
+       
+         doPostOrder(root,0,map);
+        List<List<Integer>> vertical=new ArrayList<>();
+    
+        for(Map.Entry it : map.entrySet())
+        {
+            ArrayList<Integer> front=(ArrayList<Integer>)it.getValue();
+             //Collections.sort(arr);
+            Collections.sort(front);
+            
+             vertical.add(front);
+        }
+        return vertical;
+       
+    }
+    public static void doPostOrder(TreeNode root,int line,TreeMap<Integer,ArrayList<Integer>> map)
+    {
+         if(root==null)
+             return;
+           if(map.containsKey(line))
+           {
+               ArrayList<Integer> al=map.get(line);
+               al.add(root.val);
+               map.put(line,al);
+           }
+        else
+        {
+            ArrayList<Integer> al=new ArrayList<Integer>();
+             al.add(root.val);
+            map.put(line,al);
+        }
+        doPostOrder(root.left,line-1,map);
+        doPostOrder(root.right,line+1,map);
+    }
